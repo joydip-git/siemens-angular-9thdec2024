5297e744823ee956b64a7853321ec3d10a58893b
Joydip <joydip.mondal@gmail.com>
2024-12-09 11:57:26 +0530

    day-1 introduction upload

---
+++ codes/day-1/web-app/index.js
@@ -0,0 +1,23 @@
+// const fs = require('fs')
+// const http = require('http')
+
+function clicked() {
+    //window.alert('button clicked')
+    console.log('button clicked')
+}
+// clicked()
+
+// function readData() {
+//     const data = fs.readFileSync('./data.txt', { encoding: 'utf-8' })
+//     return data
+// }
+
+// const server = http.createServer(
+//     (req, res) => {
+//         const data = readData()
+//         res.writeHead(200, 'Content-Type:application/json')
+//         res.end(`<div>${data}</div>`)
+//     }
+// )
+// server.listen(3000, () => console.log('server is running using port 3000'))
+
