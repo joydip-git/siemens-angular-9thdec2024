158e56998db05cd7cfa169cbbfce4c7e08ab7266
Joydip <joydip.mondal@gmail.com>
2024-12-09 15:29:44 +0530

    ts code upload

---
+++ codes/day-1/ts-app/index.js
@@ -0,0 +1,7 @@
+var x = 10;
+//x = 'joydip'
+function add(a, b) {
+    return a + b;
+}
+var addRes = add(12, 3);
+console.log(addRes);
---
+++ codes/day-1/ts-app/index.ts
@@ -0,0 +1,7 @@
+var x: number = 10
+//x = 'joydip'
+function add(a: number, b: number): number {
+    return a + b
+}
+const addRes = add(12, 3)
+console.log(addRes);
